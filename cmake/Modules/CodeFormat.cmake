399bbbf425b7a2cec1b5a189101920d71ca9743d
Swan BaiLei27 <65945846+Baiyi27@users.noreply.github.com>
2024-09-02 00:28:14 +0800

    <chr>(cmake):Add code formatting cmake script
    1. Only use `CodeFormat.cmake` API
    
    footer:
    1.`FindClangXXX.cmake` only able to find tools in the `VSCode user` extension directory.
    2. Ensure the `C/C++` extension has installed

---
+++ cmake/Modules/CodeFormat.cmake
@@ -0,0 +1,86 @@
+find_package(ClangFormat)
+
+set_property(GLOBAL PROPERTY clangFormatFiles)
+
+if(clangFormatExe)
+    # message(STATUS "Found clang-format: ${clangFormatExe}")
+    function(FormatCode target)
+        set(options)
+        set(oneValueArgs)
+        set(multiValueArgs EXCLUDES)
+        cmake_parse_arguments(clang_format "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+        if(TARGET ${target})
+            get_target_property(srcFiles ${target} SOURCES)
+            get_property(allSrcFiles GLOBAL PROPERTY clangFormatFiles)
+
+            foreach(fileToFormat ${srcFiles})
+                set(excluded FALSE)
+
+                foreach(exclude ${clang_format_EXCLUDES})
+                    if(${fileToFormat} MATCHES ${exclude})
+                        set(excluded TRUE)
+                        message("+++ Excluding file: ${fileToFormat} because it matches ${exclude}")
+                    endif()
+                endforeach()
+
+                if(NOT ${excluded})
+                    get_filename_component(relativeFile ${fileToFormat} ABSOLUTE)
+                    list(APPEND allSrcFiles ${relativeFile})
+                endif()
+            endforeach()
+
+            list(REMOVE_DUPLICATES allSrcFiles)
+            set_property(GLOBAL PROPERTY clangFormatFiles ${allSrcFiles})
+        else()
+            message(WARNING "Target ${target} does not exists.")
+        endif()
+    endfunction()
+
+    function(DefFormatCodeCustom)
+        if(clangFormatExe)
+            get_property(allFiles GLOBAL PROPERTY clangFormatFiles)
+            # message("+++ Formatting files: ${allFiles}")
+            set(commands)
+
+            foreach(fileToFormat ${allFiles})
+                # message("+++ Current fileToFormat: ${fileToFormat}")
+                list(APPEND commands COMMAND ${clangFormatExe} -i -style=file -fallback-style=webkit -verbose ${fileToFormat})
+            endforeach()
+
+            list(LENGTH allFiles numFiles)
+            add_custom_target(FormatCode
+                ${commands}
+                COMMENT "Format ${numFiles} source files"
+            )
+        endif()
+    endfunction()
+
+    function(DefFormatCodeAuto)
+        if(clangFormatExe)
+            get_property(allFiles GLOBAL PROPERTY clangFormatFiles)
+            # message("+++ Formatting files: ${allFiles}")
+
+            # Set up commands for clang-format
+            set(commands)
+
+            foreach(fileToFormat ${allFiles})
+                # message("+++ Current fileToFormat: ${fileToFormat}")
+                list(APPEND commands COMMAND ${clangFormatExe} -i -style=file -fallback-style=webkit -verbose ${fileToFormat})
+            endforeach()
+
+            list(LENGTH allFiles numFiles)
+            add_custom_target(
+                formatCode ALL
+                ${commands}
+                COMMENT "Format ${numFiles} source files"
+            )
+
+            # Ensure format_code is run before the build step
+            add_dependencies(${PROJECT_NAME} formatCode)
+        endif()
+    endfunction()
+
+else()
+    message(STATUS "'clang-format.exe' not found")
+endif()
