1239628dbf7a7b931b49a8eb22c6d3d343564e42
Swan BaiLei27 <65945846+Baiyi27@users.noreply.github.com>
2024-11-27 00:11:00 +0800

    <feat>(/): Add basic `main` program compilation configuration file

---
+++ lib/src/main.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#define GAME_NAME "PlantSuperSmash"
+#define VERSION   "0.0.1"
+
+int main()
+{
+    std::cout << GAME_NAME << " " << VERSION << '\n';
+
+    return 0;
+}
+
+// Date:24/10/26/2:50
